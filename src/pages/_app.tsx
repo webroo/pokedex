c463c8c2f250c06c7f640f6718c0d6e1cd95ea76
Matt Sweetman <git@webroo.org>
2020-06-29 11:19:47 +0100

    Add apollo client and implement basic query

---
+++ src/pages/_app.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { AppProps } from 'next/app';
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+} from '@apollo/client';
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:3000/api/graphql',
+  }),
+});
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
+};
+
+export default App;
